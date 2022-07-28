0ba66953f5c2323f5bb3a0b5165d32540edc64d4
Thomas Medhat Naeem <87904094+THOMASmedhat@users.noreply.github.com>
2022-07-28 17:46:23 +0200

    Update EX6.c

--- C Programing/Assignment1_U2_L3/EX_6/EX6.c
+++ C Programing/Assignment1_U2_L3/EX_6/EX6.c
@@ -1,4 +1,11 @@
-/*
+/*Write Source Code to Swap Two Numbers
+
+#########Console_output######
+Enter value of a: 1.20
+Enter value of b: 2.45
+
+After swapping, value of a = 2.45
+After swapping, value of b = 1.2
  ============================================================================
  Name        : E6.c
  Author      : Thomas Medhat
