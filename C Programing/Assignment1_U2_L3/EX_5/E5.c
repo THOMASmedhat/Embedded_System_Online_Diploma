6b13804fb4099401d14f1f775dea602a68479465
Thomas Medhat Naeem <87904094+THOMASmedhat@users.noreply.github.com>
2022-07-28 17:44:11 +0200

    Update E5.c

--- C Programing/Assignment1_U2_L3/EX_5/E5.c
+++ C Programing/Assignment1_U2_L3/EX_5/E5.c
@@ -1,4 +1,8 @@
-/*
+/*Write C Program to Find ASCII Value of a Character
+
+#########Console_output######
+Enter a character: G
+ASCII value of G = 71
  ============================================================================
  Name        : E5.c
  Author      : Thomas Medhat
